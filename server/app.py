03cef7c626fb1fdc03839b1df2fec4c02d58f73c
afshan ahmed khan <afshanahmeda2k@gmail.com>
2024-02-28 22:59:47 +0530

    Added react and gunicorn for deploying flask

--- server/app.py
+++ server/app.py
@@ -21,4 +21,9 @@
 
 from flask import Flask
 app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return "Welcome to Flight Ticket Booking System!"
+
 from controller import user, flight, admin
